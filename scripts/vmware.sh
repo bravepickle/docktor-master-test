c193804d03576a90a40c92f5ab85e7c014612aad
Victor K <bravepickle@github.com>
2015-09-23 00:26:10 +0300

    fixing vmware tools install

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -8,14 +8,24 @@ fi
 # Install the VMWare Tools from a linux ISO.
 
 #wget http://192.168.0.185/linux.iso -P /tmp
-mkdir -p /mnt/vmware
-mount -o loop /home/vagrant/linux.iso /mnt/vmware
+# mkdir -p /mnt/vmware
+# mount -o loop /home/vagrant/linux.iso /mnt/vmware
 
+# cd /tmp
+# tar xzf /mnt/vmware/VMwareTools-*.tar.gz
+
+# umount /mnt/vmware
+# rm -fr /home/vagrant/linux.iso
+
+# /tmp/vmware-tools-distrib/vmware-install.pl -d
+# rm -fr /tmp/vmware-tools-distrib
+
+# Install vmware patches
+echo Installing VMWARE Tools patches
+apt-get install git -y
 cd /tmp
-tar xzf /mnt/vmware/VMwareTools-*.tar.gz
-
-umount /mnt/vmware
-rm -fr /home/vagrant/linux.iso
-
-/tmp/vmware-tools-distrib/vmware-install.pl -d
-rm -fr /tmp/vmware-tools-distrib
+git clone https://github.com/rasa/vmware-tools-patches.git
+cd vmware-tools-patches
+./patched-open-vm-tools.sh
+rm -fr /tmp/vmware-tools-patches
+echo Finished Patching...
