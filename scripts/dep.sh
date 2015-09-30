bccb59d80bb49e4ce5e7e3106e261014cfb5bbaa
Victor K <bravepickle@github.com>
2015-09-30 22:08:41 +0300

    update configs

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -24,7 +24,8 @@ puppet --version || (\
     wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb && \
     dpkg -i puppetlabs-release-trusty.deb && \
     apt-get update && \
-    apt-get install puppet-common -y)
+    apt-get install puppet-common -y && \
+    puppet --version)
 
 echo Setup Docker Compose
 docker-compose --version || (\
