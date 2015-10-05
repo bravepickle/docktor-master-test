5b298516d0b034f462a2c408fa46275a3862f20e
Victor K <bravepickle@github.com>
2015-10-05 22:37:25 +0300

    extending configs

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -4,6 +4,8 @@
 
 apt-get -y update
 
+DEBIAN_FRONTEND=noninteractive
+
 apt-get -y install curl wget
 
 # Deploy docker master app
@@ -36,4 +38,17 @@ echo Setup SupervisorD
 supervisord --version || (apt-get install supervisor -y)
 
 echo Setup NGINX
-nginx --version || (apt-get install nginx -y) && nginx --version
+nginx -v || (wget -q http://nginx.org/keys/nginx_signing.key -O- | apt-key add - && \
+    echo deb http://nginx.org/packages/ubuntu/ trusty nginx >> /etc/apt/sources.list && \
+    echo deb-src http://nginx.org/packages/ubuntu/ trusty nginx >> /etc/apt/sources.list && \
+    apt-get update && \
+    apt-get -y upgrade && \
+    sed -i -e"s/worker_processes  1/worker_processes 5/" /etc/nginx/nginx.conf && \
+    sed -i -e"s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf && \
+    sed -i -e"s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 100m/" /etc/nginx/nginx.conf && \
+    sed -i "s/.*conf\.d\/\*\.conf;.*/&\n    include \/etc\/nginx\/sites-enabled\/\*;/" /etc/nginx/nginx.conf && \
+    rm -Rf /etc/nginx/conf.d/* && \
+    mkdir -p /etc/nginx/sites-available/ && \
+    mkdir -p /etc/nginx/sites-enabled/ && \
+    mkdir -p /etc/nginx/ssl/ && \
+    nginx -v)
